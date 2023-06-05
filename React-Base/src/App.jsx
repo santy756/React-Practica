d3fa52860ec68ca446586c8873aea70b47abf66f
santy756 <santyperez56@gmail.com>
2023-06-04 21:32:07 -0300

    React

---
+++ React-Base/src/App.jsx
@@ -0,0 +1,8 @@
+import './App.css'
+import Noticia from './components/noticia'
+
+function App() {
+  return <Noticia/>
+}
+
+export default App
---
+++ React-Base/src/components/Noticia.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Noticia() {
+    return <div>
+        <figure>
+            <img src="https://scontent.ftuc4-2.fna.fbcdn.net/v/t39.30808-6/223100808_186944486804891_4068073180784434428_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=973b4a&_nc_ohc=8cxhMLTJURAAX97J6Am&_nc_ht=scontent.ftuc4-2.fna&oh=00_AfCAiOAQrqIBnc5FYmRe15PbMkzaxoiYnZjukEG_Z4zz-g&oe=647E80DB" alt="" />
+            <figcaption>Senku Ishigami - Protagonista Dr Stone</figcaption>
+        </figure>
+        <p>El protagonistade Dr Stone hace referencia en su ultimo capitulo la iconica fotografia del cientifico Albert Einstein </p>
+    </div>
+};
+
+export default Noticia;
